c25b2db57c3d5e195426cd2b62cd3f309a372070
AldeineRielCruz <cruz.aldeineriel@gmail.com>
2025-12-01 09:32:08 +0800

    Update server.js
    idk doesnt get used

--- server.js
+++ server.js
@@ -1,34 +1,17 @@
-const http= require("http");
-const PORT=2800;
-const hostname="127.0.0.1";
-const fs= require("fs");
 var express = require('express');
-var path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+var app = express();
 
-const server=http.createServer(function(req,res){
+// set the port of our application
+var port = process.env.PORT || 8080;
 
-    //res.write("Hello User from the server");
-    //res.end();
-    res.writeHead(200,{"Content-type":"text/html"});
-    fs.readFile("index.html",function(error,data){
-        if (error){
-            res.writeHead(404);
-            res.write("Error, file not found!");
-        }else{
-            res.write(data);
-        }
-        res.end();
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname+"/web"));
 
-
+// set the home page route
+app.get('/', function(req, res) {
+    res.sendFile('index.html');
 });
-})
 
-server.listen(PORT,function (error){
-    if(error){
-        console.log(`You have an error, ${error}`)
-    }else {
-        console.log(`Server is running' http://${hostname}:${PORT}`);
-        
-    }
+app.listen(port, function() {
+    console.log('Our app is running on ' + port);
 });
